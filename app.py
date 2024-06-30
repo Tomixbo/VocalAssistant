20d752a88ceab28d93561ba8dc9dbe86686cdcec
Tomixbo <tomixbo@gmail.com>
2024-06-30 22:01:38 +0300

    ajout : assistant API, multisession handling, gpt-4o

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import streamlit as st
 import time
-from utils import get_answer, text_to_speech, autoplay_audio, speech_to_text, sticky_footer, sticky_header
+from utils import get_answer, text_to_speech, autoplay_audio, speech_to_text, sticky_footer, sticky_header, init_model
 import os
 from audio_recorder_streamlit import audio_recorder
 import base64
+import uuid
 
-# Initialize session state for managing chat messages
+# FUNCTION INITIALIZE session_state : messages, audio_bytes, loaded
 def initialize_session_state(message_init):
+    if "session_id" not in st.session_state:
+        st.session_state.session_id = str(uuid.uuid4())
+    
     if "messages" not in st.session_state:    
         st.session_state.messages = [{"role": "assistant", "content": message_init}]   
         audio_file = text_to_speech(message_init)
@@ -20,7 +24,13 @@ def initialize_session_state(message_init):
         """
         st.markdown(md, unsafe_allow_html=True)
         time.sleep(1)
-        os.remove(audio_file)   
+        os.remove(audio_file) 
+    
+    if "thread" not in st.session_state:
+        st.session_state.thread = init_model(st.secrets["ASSISTANT_ID"])
+    else:
+        st.session_state.thread = init_model(st.secrets["ASSISTANT_ID"], st.session_state.thread.id)
+    
     if 'audio_bytes' not in st.session_state:
         st.session_state.audio_bytes = None
     if 'loaded' not in st.session_state:
@@ -29,13 +39,13 @@ def initialize_session_state(message_init):
 message_init = "Bonjour, comment je peux vous aider?"
 initialize_session_state(message_init)
 
-# Main content container
+# HEADER STICKY
 with st.container():
     st.title("Assistant Vocal 🤖")
     st.write("Bienvenue, appuyez sur l'icone d'enregistrement pour interagir avec votre assistant.")
     sticky_header()
 
-# Placeholder for chat messages
+# PLACEHOLDER FOR MESSAGES
 chat_placeholder = st.container()
 
 with chat_placeholder:
@@ -43,27 +53,22 @@ with chat_placeholder:
         with st.chat_message(message["role"]):
             st.write(message["content"])
 
+# RERUN APP
+if not st.session_state.loaded:
+    # Introduce a small delay
+    time.sleep(1)
+    st.session_state.loaded = True
+    st.experimental_rerun()
 
-# message status
+# FOOTER CONTAINER
+footer = st.container()
+# init message status
 message_status = None
 
-# Create a container for the sticky footer
-footer = st.container()
-
-# Add content to the footer
+# FOOTER STICKY - MESSAGE INPUT - AUDIO RECORDER
 with footer:
     sticky_footer()
-
-    # Only display the audio recorder after the initial load
-    if not st.session_state.loaded:
-        # Introduce a small delay
-        time.sleep(1)
-        st.session_state.loaded = True
-        # Ajouter un script JavaScript pour redimensionner la fenêtre après le chargement de la page
-        
-        st.rerun()
-    
-    col1, col2= st.columns([11, 1], vertical_alignment="center")
+    col1, col2 = st.columns([11, 1], vertical_alignment="center")
     with col1:
         if prompt := st.chat_input("Write your message here..."):
             st.session_state.messages.append({"role": "user", "content": prompt})
@@ -73,17 +78,14 @@ with footer:
                     st.write(prompt)
     with col2:
         st.session_state.audio_bytes = audio_recorder(text="", icon_size="2x")
-    
 
-
-
-# Process the recorded audio if available
+# SPEECH TO TEXT
 with chat_placeholder: 
     if st.session_state.audio_bytes and not message_status:
         # Conteneur pour le spinner spécifique
         with st.spinner("Transcribing..."):
             # Write the audio bytes to a temporary file
-            webm_file_path = "temp_audio.mp3"
+            webm_file_path = f"temp_audio_{st.session_state.session_id}.mp3"
             with open(webm_file_path, "wb") as f:
                 f.write(st.session_state.audio_bytes)
 
@@ -96,14 +98,13 @@ with chat_placeholder:
                     st.write(transcript)
                     os.remove(webm_file_path)
             st.session_state.audio_bytes = None
-             
 
 # RESPONSE
 if st.session_state.messages[-1]["role"] != "assistant":
     with chat_placeholder:
         with st.chat_message("assistant"):
             with st.spinner("Thinking🤔..."):
-                final_response = get_answer(st.session_state.messages)
+                final_response = get_answer(st.session_state.messages[-1]['content'], st.session_state.thread)
             with st.spinner("Generating audio response..."):
                 audio_file = text_to_speech(final_response)
                 autoplay_audio(audio_file)
@@ -111,5 +112,3 @@ if st.session_state.messages[-1]["role"] != "assistant":
             st.session_state.messages.append({"role": "assistant", "content": final_response})
             os.remove(audio_file)
             message_status = None
-
-
